32b4b0309cb94c7c28e2663ea17c8d9f5db78ac2
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2019-11-21 22:14:44 +0600

    add code html & other small improvement

--- packages/analytics/urls.py
+++ packages/analytics/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import home
+from .views import home, add_code_to_site
 
 app_name = 'analytics'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('campaign/add-code/', add_code_to_site, name='add-code'),
 ]
--- packages/analytics/views.py
+++ packages/analytics/views.py
@@ -4,5 +4,9 @@ from django.shortcuts import render
 
 @login_required
 def home(request):
-    return render(request, 'dashboard/home.html')
+    return render(request, 'analytics/home.html')
 
+
+@login_required
+def add_code_to_site(request):
+    return render(request, 'analytics/add-code-to-site.html')
