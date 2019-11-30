07ad1f8a4ff9df9c674ba4e921eb3e550ad7dc8c
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2019-12-01 03:22:44 +0600

    added signals for site visit count according to page visit

--- packages/analytics/apps.py
+++ packages/analytics/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class AnalyticsConfig(AppConfig):
     name = 'analytics'
+
+    def ready(self):
+        import analytics.signals
+
---
+++ packages/analytics/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import PageVisit, SiteVisit
+
+
+@receiver(post_save, sender=PageVisit)
+def add_site_visit_count(sender, instance, created, **kwargs):
+    if created:
+        SiteVisit.objects.create(
+            site=instance.page.host,
+            ip_addr=instance.ip_addr,
+        )
+
+
+
+
